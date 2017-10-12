caee14f23603f64973da1530bb8d8cfef9a51556
Anneka Bath <anneka.griffith@g.austincc.edu>
2017-10-12 18:30:46 -0500

    Add files via upload

---
+++ ComplexNums.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quadraticformula;
+
+/**
+ *
+ * @author ozzma
+ */
+public class ComplexNums { 
+     //****************************
+    //set private variables a,b,&c 
+    //****************************
+    private double a,b,c;
+    public ComplexNums(double a, double b, double c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    //********************
+    //get calls for a,b,&c
+    //********************
+    public double getA()
+    {
+        return a;
+    }
+    public double getB()
+    {
+        return b;
+    }
+    public double getC()
+    {
+        return c;
+    }
+    //********************
+    //set calls for a,b,&c
+    //********************
+    
+}
--- QuadraticFormula.java
+++ QuadraticFormula.java
@@ -14,49 +14,33 @@ import java.util.Scanner;
  * @author Anneka Bath
  * date 10/23/17
  */
+
+   
 public class QuadraticFormula {
-    //****************************
-    //set private variables a,b,&c 
-    //****************************
-    private double a,b,c;
-    //********************
-    //get calls for a,b,&c
-    //********************
-    public double getA()
-    {
-        return a;
-    }
-    public double getB()
-    {
-        return b;
-    }
-    public double getC()
-    {
-        return c;
-    }
-    //********************
-    //set calls for a,b,&c
-    //********************
-    public void setA(double a)
-    {
-        this.a = a;
-    }
-    public void setB(double b)
-    {
-        this.b = b;
-    }
-    public void setC(double c)
-    {
-        this.c = c;
-    }
+    
+    double p; //root1
+    double q; //root2
+    double imag; //imagionary number
+    
     //**************************************
     //calculate the discriminant, (b^2 - 4ac)
     //**************************************
-    public double calDiscrim(double a, double b, double c)
+    public double calDiscrim( double a , double b, double c)
     {
         double discrim;
         discrim = b*b - 4*a*c;
         return discrim;
+    }
+    //**********************************
+    // Get calls for both roots p and q
+    //**********************************
+    public double Getp ()
+    {
+        return p;
+    }
+    public double Getq ()
+    {
+        return q;
     }
     //********************************************************************
     // evaluate the quadratic equatioionby finding the roots. 
@@ -65,47 +49,58 @@ public class QuadraticFormula {
     //********************************************************************
     public void solveRoots (double discrim, double a, double b, double c)
     {
-        double root1;
-        double root2;
-        double imag;
-    
         // modify output depending on sign of discriminant
         if (discrim > 0) //two real roots
         {
-            root1 = (-b + Math.sqrt(discrim))/(2*a);
-            root2 = (-b - Math.sqrt(discrim))/(2*a);
-            System.out.println("Two real roots: " + root1 + " and " + root2);
+            p = (-b + Math.sqrt(discrim))/(2*a);
+            q = (-b - Math.sqrt(discrim))/(2*a);
+            System.out.println("Two real roots: p = " + p + " and q = " + q + "\n");
         }
         else if(discrim < 0)// two imagionary roots 
         { 
             imag = Math.sqrt(-discrim) /(2*a);
-            System.out.println("Two imagionary roots: " + (-b/(2*a)) + "+/- i" + imag);
+            System.out.println("Two imagionary roots: " + (-b/(2*a)) + "+/- i" + imag+ "\n");
         }
         else {// one real roots 
-            root1 = -b/(2*a);
-            System.out.println("One real root: " + root1);
+            p = -b/(2*a);
+            System.out.println("One real root: p = " + p + "\n");
         }
+    }
+    //*******************************************************
+    //Root Checker: makes sure that p*q = c/a & p+q = -(b/a)
+    //*******************************************************
+    public void checkRoots (double a, double b, double c, double p, double q)
+    {
+        System.out.println("Checking if the roots p and q are correct:");
+        double check1 = p*q; 
+        double answer1 = c/a;
+        System.out.println("roots p*q = " + check1 + " Which equal c/a = " + answer1);
+        double check2 = p+q;
+        double answer2 = (-b)/a;
+        System.out.println("roots p+q = " + check2 + " Which equal -(b/a) = " + answer2);
     }
     //**************
     //main
     //**************
     public static void main(String[] args) {
         // TODO code application logic here
-        QuadraticFormula quad = new QuadraticFormula();
+        
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("The Quadratic formula finds teh root(s) \n" + 
+        System.out.println("The Quadratic Equation formula finds the root(s) \n" + 
                 "where f(x) = 0 for the function f(x) = ax^2 + bx +c\n");
 
         System.out.print("a: ");
         double a =scan.nextDouble();
-        quad.setA(a);
         System.out.print("b: ");
         double b = scan.nextDouble();
-        quad.setB(b);
         System.out.print("c: ");
         double c = scan.nextDouble();
-        quad.setC(c);
+        ComplexNums nums = new ComplexNums(a,b,c); 
+        QuadraticFormula quad = new QuadraticFormula();
+        double discrim = quad.calDiscrim(nums.getA(),nums.getB(),nums.getC());
+        quad.solveRoots(discrim, nums.getA(),nums.getB(),nums.getC());
+        quad.checkRoots(nums.getA(),nums.getB(),nums.getC(), quad.Getp(), quad.Getq());
     }
 }
 
